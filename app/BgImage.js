bd0d9f8f9842dc681f12457d8ec5345b6ea4e577
Rafa Lyovson <lyovson@icloud.com>
2023-03-26 19:15:57 +0400

    deleted the app directory outside of the src

--- app/BgImage.js
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function bgImage() {
-  return (
-    <div className="fixed w-screen h-screen -z-10 overflow-hidden blur-sm">
-      <Image className="sepia" src="/images/cast.jpeg" fill="true" alt="OTTK" />
-    </div>
-  );
-}
